869f9e997ef96aa32c9d492ecf8c703d1e0fd66f
Ozan <ozancangu24@gmail.com>
2023-02-13 18:28:55 +0300

    -list fragment menu created

--- app/src/main/java/com/ozancanguz/diaryapp/ui/fragments/list/ListFragment.kt
+++ app/src/main/java/com/ozancanguz/diaryapp/ui/fragments/list/ListFragment.kt
@@ -1,10 +1,8 @@
 package com.ozancanguz.diaryapp.ui.fragments.list
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.ozancanguz.diaryapp.R
 import com.ozancanguz.diaryapp.databinding.FragmentListBinding
@@ -24,10 +22,18 @@ class ListFragment : Fragment() {
          _binding = FragmentListBinding.inflate(inflater, container, false)
         val view = binding.root
 
+        // setMenu
+        setHasOptionsMenu(true)
+
       binding.floatingActionButton.setOnClickListener {
           findNavController().navigate(R.id.action_listFragment_to_addFragment)
       }
         return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.listfragment_menu,menu)
+        super.onCreateOptionsMenu(menu, inflater)
     }
 
 
