1c3c6decd2ca103ec90da859cd596d3db61cad32
Ozan <ozancangu24@gmail.com>
2023-02-13 18:38:12 +0300

    -update fragment menu created

--- app/src/main/java/com/ozancanguz/diaryapp/ui/fragments/update/UpdateFragment.kt
+++ app/src/main/java/com/ozancanguz/diaryapp/ui/fragments/update/UpdateFragment.kt
@@ -1,10 +1,8 @@
 package com.ozancanguz.diaryapp.ui.fragments.update
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.ozancanguz.diaryapp.R
 import com.ozancanguz.diaryapp.databinding.FragmentUpdateBinding
 
@@ -22,9 +20,16 @@ class UpdateFragment : Fragment() {
         // Inflate the layout for this fragment
           _binding = FragmentUpdateBinding.inflate(inflater, container, false)
         val view = binding.root
+
+        // set menu
+        setHasOptionsMenu(true)
         return view
 
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.update_fragment_menu,menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
 
 }
