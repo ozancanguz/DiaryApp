1a249c4bf0b9db46b3ee6577e2507d9928b9880b
Ozan <ozancangu24@gmail.com>
2023-02-13 18:32:42 +0300

    -add fragment menu created

--- app/src/main/java/com/ozancanguz/diaryapp/ui/fragments/add/AddFragment.kt
+++ app/src/main/java/com/ozancanguz/diaryapp/ui/fragments/add/AddFragment.kt
@@ -1,10 +1,8 @@
 package com.ozancanguz.diaryapp.ui.fragments.add
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.ozancanguz.diaryapp.R
 import com.ozancanguz.diaryapp.databinding.FragmentAddBinding
@@ -25,8 +23,15 @@ class AddFragment : Fragment() {
         val view = binding.root
 
 
+        // set menu
+        setHasOptionsMenu(true)
 
         return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.add_fragment_menu,menu)
+        super.onCreateOptionsMenu(menu, inflater)
     }
 
 
