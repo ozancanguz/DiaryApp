17462db9963bc7568d8767cff4357c6077a8b482
Ozan <ozancangu24@gmail.com>
2023-02-13 19:03:35 +0300

    -In addFragment viewmodel function implemented

--- app/src/main/java/com/ozancanguz/diaryapp/ui/fragments/add/AddFragment.kt
+++ app/src/main/java/com/ozancanguz/diaryapp/ui/fragments/add/AddFragment.kt
@@ -3,10 +3,12 @@ package com.ozancanguz.diaryapp.ui.fragments.add
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.ozancanguz.diaryapp.R
 import com.ozancanguz.diaryapp.data.model.Diary
 import com.ozancanguz.diaryapp.databinding.FragmentAddBinding
+import com.ozancanguz.diaryapp.viewmodel.DiaryViewModel
 import kotlinx.android.synthetic.main.fragment_list.*
 
 
@@ -14,6 +16,8 @@ class AddFragment : Fragment() {
       private var _binding: FragmentAddBinding? = null
 
     private val binding get() = _binding!!
+
+    private val diaryViewModel:DiaryViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,11 +39,19 @@ class AddFragment : Fragment() {
         super.onCreateOptionsMenu(menu, inflater)
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId==R.id.menu_add){
+          insertToDb()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
     fun insertToDb(){
         val title=binding.titleEt.text.toString()
         val description=binding.descriptionEt.text.toString()
-
         var newData= Diary(title,description)
+        diaryViewModel.insertData(newData)
+        findNavController().navigate(R.id.action_addFragment_to_listFragment)
     }
 
 
