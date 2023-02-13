daf2435181836bdb2af2d54e2e314f9533b39ece
Ozan <ozancangu24@gmail.com>
2023-02-13 18:43:37 +0300

    -Diary Dao created

---
+++ app/src/main/java/com/ozancanguz/diaryapp/data/db/DiaryDao.kt
@@ -0,0 +1,23 @@
+package com.ozancanguz.diaryapp.data.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.ozancanguz.diaryapp.data.model.Diary
+
+@Dao
+interface DiaryDao {
+
+
+    // get all diary from db
+    @Query("select * from diary_table order by id Asc")
+    suspend fun getAllData(): LiveData<List<Diary>>
+
+
+    // insert diary
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertData(diary: Diary)
+
+}
