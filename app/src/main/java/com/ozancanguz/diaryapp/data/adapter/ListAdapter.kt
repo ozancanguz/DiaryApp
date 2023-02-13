f674973d6bf0fada8b2e5c41d2292ef33162e8c6
Ozan <ozancangu24@gmail.com>
2023-02-13 19:06:17 +0300

    -diary list adapter created

---
+++ app/src/main/java/com/ozancanguz/diaryapp/data/adapter/ListAdapter.kt
@@ -0,0 +1,47 @@
+package com.ozancanguz.diaryapp.data.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.ozancanguz.diaryapp.R
+import com.ozancanguz.diaryapp.data.model.Diary
+import kotlinx.android.synthetic.main.list_row_layout.view.*
+
+class ListAdapter: RecyclerView.Adapter<ListAdapter.MyViewHolder>() {
+
+
+    var todolist= emptyList<Diary>()
+
+
+    // update ui
+    fun updateData(newData:List<Diary>){
+        this.todolist=newData
+        notifyDataSetChanged()
+    }
+
+    class MyViewHolder(view: View): RecyclerView.ViewHolder(view){
+
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val inflater= LayoutInflater.from(parent.context)
+        val view=inflater.inflate(R.layout.list_row_layout,parent,false)
+        return MyViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+
+        val currentItem=todolist[position]
+        holder.itemView.title_txt.text=currentItem.title
+        holder.itemView.description_txt.text=currentItem.description
+
+    }
+
+    override fun getItemCount(): Int {
+        return todolist.size
+    }
+
+
+}
