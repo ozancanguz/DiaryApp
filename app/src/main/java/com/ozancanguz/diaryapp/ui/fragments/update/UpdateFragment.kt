b09a98c8769f5d60219d3f1a1464710d30bcf3af
Ozan <ozancangu24@gmail.com>
2023-02-13 19:38:20 +0300

    -update item function created on UpdateFragment.kt

--- app/src/main/java/com/ozancanguz/diaryapp/ui/fragments/update/UpdateFragment.kt
+++ app/src/main/java/com/ozancanguz/diaryapp/ui/fragments/update/UpdateFragment.kt
@@ -3,9 +3,12 @@ package com.ozancanguz.diaryapp.ui.fragments.update
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import com.ozancanguz.diaryapp.R
+import com.ozancanguz.diaryapp.data.model.Diary
 import com.ozancanguz.diaryapp.databinding.FragmentUpdateBinding
+import com.ozancanguz.diaryapp.viewmodel.DiaryViewModel
 
 
 class UpdateFragment : Fragment() {
@@ -14,6 +17,9 @@ class UpdateFragment : Fragment() {
     private val binding get() = _binding!!
 
     private val args:UpdateFragmentArgs by navArgs()
+
+    //init viewmodel
+    private val viewmodel:DiaryViewModel by viewModels()
 
 
     override fun onCreateView(
@@ -28,16 +34,27 @@ class UpdateFragment : Fragment() {
         setHasOptionsMenu(true)
 
 
-        updateUi()
+        getArgs()
 
 
         return view
 
 
 
+
     }
 
-    private fun updateUi() {
+    private fun updateItem() {
+        val title=binding.currentTitleEt.text.toString()
+        val description=binding.currentDescriptionEt.text.toString()
+
+        val updatedItem=Diary(title,description)
+
+
+
+    }
+
+    private fun getArgs() {
         binding.currentTitleEt.setText(args.currentDiary.title)
         binding.currentDescriptionEt.setText(args.currentDiary.description)
     }
@@ -45,6 +62,14 @@ class UpdateFragment : Fragment() {
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.update_fragment_menu,menu)
         super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if(item.itemId==R.id.menu_save){
+            updateItem()
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 }
