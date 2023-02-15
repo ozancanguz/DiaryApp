77bd2e9f51d4368f1ba962bfa4d5dfbd3ef438ac
Ozan <ozancangu24@gmail.com>
2023-02-15 16:54:05 +0300

    -swipe to delete function created

--- app/src/main/java/com/ozancanguz/diaryapp/ui/fragments/list/ListFragment.kt
+++ app/src/main/java/com/ozancanguz/diaryapp/ui/fragments/list/ListFragment.kt
@@ -2,15 +2,19 @@ package com.ozancanguz.diaryapp.ui.fragments.list
 
 import android.os.Bundle
 import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.ozancanguz.diaryapp.R
 import com.ozancanguz.diaryapp.data.adapter.ListAdapter
 import com.ozancanguz.diaryapp.databinding.FragmentListBinding
+import com.ozancanguz.diaryapp.util.SwipeToDelete
 import com.ozancanguz.diaryapp.viewmodel.DiaryViewModel
 import kotlinx.android.synthetic.main.fragment_list.*
 
@@ -55,19 +59,36 @@ class ListFragment : Fragment() {
         diaryViewModel.getAllData.observe(viewLifecycleOwner, Observer {
             listAdapter.updateData(it)
         })
-        binding.noDataTextView.visibility=View.INVISIBLE
-
     }
 
     private fun setupRv() {
          binding.recyclerView.layoutManager=StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL)
         binding.recyclerView.adapter=listAdapter
+        swipeToDelete(binding.recyclerView)
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.listfragment_menu,menu)
         super.onCreateOptionsMenu(menu, inflater)
     }
+
+    //swipe to delete
+    private fun swipeToDelete(recyclerView: RecyclerView) {
+
+        val swipeToDeleteCallback=object : SwipeToDelete(){
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val itemtoDelete=listAdapter.todolist[viewHolder.adapterPosition]
+                diaryViewModel.deleteSingleItem(itemtoDelete)
+                Toast.makeText(requireContext(),"Removed successfully", Toast.LENGTH_LONG).show()
+
+
+            }
+        }
+        val itemTouchHelper= ItemTouchHelper(swipeToDeleteCallback)
+        itemTouchHelper.attachToRecyclerView(recyclerView)
+    }
+
 
 
 }
